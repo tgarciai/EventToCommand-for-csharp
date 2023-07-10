3efb15dc27b8d3879f35941dfb6522a4553abe9e
Tirso Garcia <tgarciaib@gmail.com>
2023-07-10 16:45:35 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,22 +8,21 @@ namespace commands
         static void Main(string[] args)
         {
          
-			EventToCommandMapper commandToEventMapper = new EventToCommandMapper();
+		EventToCommandMapper commandToEventMapper = new EventToCommandMapper();
 
-			TestCommandA commandA = new TestCommandA();
-            TestCommandB commandB = new TestCommandB();
-			commandToEventMapper.MapCommand(commandA,TestEventA.TYPE);
-            commandToEventMapper.MapCommand(commandB,TestEventB.TYPE);
+		TestCommandA commandA = new TestCommandA();
+            	TestCommandB commandB = new TestCommandB();
+		commandToEventMapper.MapCommand(commandA,TestEventA.TYPE);
+            	commandToEventMapper.MapCommand(commandB,TestEventB.TYPE);
 
+		var evtA = new TestEventA(TestEventA.TYPE,"hello");
+            	var evtB = new TestEventA(TestEventB.TYPE,"world");
 
-			var evtA = new TestEventA(TestEventA.TYPE,"hello");
-            var evtB = new TestEventA(TestEventB.TYPE,"world");
+		evtA.dispatch();
+            	evtB.dispatch();
 
-			evtA.dispatch();
-            evtB.dispatch();
-
-            commandToEventMapper.RemoveCommand(commandA,TestEventA.TYPE);
-            commandToEventMapper.RemoveCommand(commandB,TestEventB.TYPE);
+            	commandToEventMapper.RemoveCommand(commandA,TestEventA.TYPE);
+            	commandToEventMapper.RemoveCommand(commandB,TestEventB.TYPE);
 
 
         }
