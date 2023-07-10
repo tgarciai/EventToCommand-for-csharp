71eafd41a358c053dce3d7a2c1721f57ecaadc00
Tirso Garcia <tgarciaib@gmail.com>
2023-07-10 16:47:10 +0200

    Update TestEventA.cs

--- TestEventA.cs
+++ TestEventA.cs
@@ -3,13 +3,10 @@ namespace commands
 {
     public class TestEventA : CommandEvent
     {
-
         public static string TYPE = "TEST_EVENT_A";
         public TestEventA(string type, object data = null) : base(type, data)
         {
 
         }
-
-
     }
 }
